4f6aeae8c5c0a2142156a0fbb0c1921573e4f04a
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-01-26 14:10:38 +0100

    Update CharacterSelection.hx

--- source/CharacterSelection.hx
+++ source/CharacterSelection.hx
@@ -159,8 +159,9 @@ class CharacterSelection extends MusicBeatState
                 {
                     alreadySelected = true;
                     var daSelected:String = menuItems[curSelected];
+                    PlayState.hasPlayedOnce = true;
                     if (menuItems[curSelected] != 'bf')
-                        PlayState.bfsel = daSelected;
+                        PlayState.SONG.player1 = daSelected;
 
                     FlxFlicker.flicker(iconArray[curSelected], 0);
                     new FlxTimer().start(1, function(tmr:FlxTimer)
