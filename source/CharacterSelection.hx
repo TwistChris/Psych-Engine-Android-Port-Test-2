21ba351f5476492e665070eebc2c414519163001
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-01-27 16:56:01 +0100

    Update CharacterSelection.hx

--- source/CharacterSelection.hx
+++ source/CharacterSelection.hx
@@ -76,13 +76,11 @@ class CharacterSelection extends MusicBeatState
             //songText.x += 40;
             //DON'T PUT X IN THE FIRST PARAMETER OF new ALPHABET()!
             //songText.screenCenter(X);
-            var icon:Boyfriend = new Boyfriend(0, 0, menuItems[i]);
 
             icon.scale.set(0.8, 0.8);
 
             //Using a FlxGroup is too much fuss!
             iconArray.push(icon);
-            add(icon);
 
             var offset:Float = 108 - (Math.max(menuItems.length, 4) - 4) * 80;
 	    shitCharacter = new FlxSprite(0, (i * 140)  + offset);
