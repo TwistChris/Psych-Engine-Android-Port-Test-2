1f815088e80c2e55617655eb4c8566e04a396922
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-01-27 17:07:49 +0100

    Update CharacterSelection.hx

--- source/CharacterSelection.hx
+++ source/CharacterSelection.hx
@@ -76,6 +76,7 @@ class CharacterSelection extends MusicBeatState
             //songText.x += 40;
             //DON'T PUT X IN THE FIRST PARAMETER OF new ALPHABET()!
             //songText.screenCenter(X);
+            var icon:Boyfriend = new Boyfriend(0, 0);
 
             icon.scale.set(0.8, 0.8);
 
