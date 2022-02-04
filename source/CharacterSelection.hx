54512526faf26706409565d305f7bfa9b1de82a2
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-02-04 23:49:39 +0100

    Update CharacterSelection.hx

--- source/CharacterSelection.hx
+++ source/CharacterSelection.hx
@@ -142,7 +142,7 @@ class CharacterSelection extends MusicBeatState
 
     public function addColor(color:Int)
     {
-        menuItems.push(new SongMetadata(color));
+        menuItems.push(new CharSelect(color));
     }
 
     override function update(elapsed:Float) 
