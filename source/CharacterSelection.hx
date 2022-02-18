10e9a444451acba63abe4319fe891230a01b2265
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-02-18 23:58:57 +0100

    Update CharacterSelection.hx

--- source/CharacterSelection.hx
+++ source/CharacterSelection.hx
@@ -234,7 +234,7 @@ class CharacterSelection extends MusicBeatState
                 doesntExist = false;
                 var storedColor:FlxColor = 0xFFFFFF;
                 remove(icon);
-                menuBG.color = nameColors;
+                menuBG.color = nameColors[curSelected];
 
                 doesntExist = true;
 
