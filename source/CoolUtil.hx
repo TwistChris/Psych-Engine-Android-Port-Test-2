aedd8472c4119038529c5415723bc48b3cfd7733
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-02-19 11:46:19 +0100

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -42,8 +42,9 @@ class CoolUtil
 
 		return daList;
 	}
-        //get the most used color in an image
-	public static function dominantColor(sprite:FlxSprite):Int{
+
+	public static function dominantColor(sprite:FlxSprite):Int
+        {
 		var countByColor:Map<Int, Int> = [];
 		for(col in 0...sprite.frameWidth){
 			for(row in 0...sprite.frameHeight){
