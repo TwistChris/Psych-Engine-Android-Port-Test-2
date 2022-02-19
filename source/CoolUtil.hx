57aea90754a81736b94308063f93f0599ba59529
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-02-19 11:36:04 +0100

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -69,6 +69,33 @@ class CoolUtil
 		return maxKey;
 	}
 
+        public static function leastColor(sprite:FlxSprite) //return the least used color in an image
+	{
+		var countByColor:Map<Int, Int> = [];
+		for(col in 0...sprite.frameWidth){
+			for(row in 0...sprite.frameHeight){
+			  var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
+			  if(colorOfThisPixel != 0){
+				  if(countByColor.exists(colorOfThisPixel)){
+				    countByColor[colorOfThisPixel] =  countByColor[colorOfThisPixel] + 1;
+				  }else if(countByColor[colorOfThisPixel] != 13520687 - (2*13520687)){
+					 countByColor[colorOfThisPixel] = 1;
+				  }
+			  }
+			}
+		 }
+		var minCount = 0;
+		var minKey:Int = 0;//after the loop this will store the min color
+		countByColor[FlxColor.BLACK] = 0;
+			for(key in countByColor.keys()){
+			if(countByColor[key] <= minCount){
+				minCount = countByColor[key];
+				minKey = key;
+			}
+		}
+		return minKey;
+	}
+
 	public static function numberArray(max:Int, ?min = 0):Array<Int>
 	{
 		var dumbArray:Array<Int> = [];
