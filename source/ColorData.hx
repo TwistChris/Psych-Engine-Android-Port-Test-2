4ccd90f51b09b554222293635d25e55506440aee
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-02-20 10:38:10 +0100

    Update ColorData.hx

--- source/ColorData.hx
+++ source/ColorData.hx
@@ -9,7 +9,7 @@ class ColorData {
       var nameColors:Array<String> = [];
       public var color:Int = -7179779;
 
-      public static function coolColorFile:(Array<String>)
+      public static function coolColorFile:()Array<String>
       {
 		CoolUtil.coolTextFile(Paths.txt('colors'));
       }
