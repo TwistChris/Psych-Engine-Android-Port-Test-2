423020928824eae37776c3aa48106e46c7d1a6b5
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-02-25 23:48:04 +0100

    Update ColorData.hx

--- source/ColorData.hx
+++ source/ColorData.hx
@@ -6,31 +6,10 @@ import openfl.utils.Assets as OpenFlAssets;
 using StringTools;
 
 class ColorData {
-      var nameColors:Array<String> = [];
       public var color:Int = -7179779;
-      public var colorfolder:Array<String> = [];
 
-      public static function coolColorFile(path:String):Array<String>
-      {
-	      var daList:Array<String> = [];
-              if(Assets.exists(path)) daList = Assets.getText(path).trim().split('\n');
-
-	      for (i in 0...daList.length)
-	      {
-		      daList[i] = daList[i].trim();
-	      }
-
-	      return daList;
-      }
-
-      public static function okk(nameColors:Array<String>)
-      {
-              CoolUtil.coolTextFile(Paths.txt('colors'));
-      }
-
-      public function new(color:Int, colorfolder:Array<String>)
+      public function new(color:Int)
       {
 	      this.color = color;
-              this.colorfolder = colorfolder;
       }
 }
