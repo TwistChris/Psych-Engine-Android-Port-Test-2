ba99919553615b7e9934a5d025ae1c096114d386
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-03-01 01:08:50 +0100

    Update ColorData.hx

--- source/ColorData.hx
+++ source/ColorData.hx
@@ -1,9 +1,28 @@
 package;
 
+import flixel.FlxG;
 import lime.utils.Assets;
+import lime.utils.Assets as LimeAssets;
+import lime.utils.AssetLibrary;
+import lime.utils.AssetManifest;
 import openfl.utils.Assets as OpenFlAssets;
+import openfl.utils.Assets;
 
 using StringTools;
+
+public static function coolColorFile(path:String):Array<String>
+	{
+		var daList:Array<String> = [];
+		if(Assets.exists(path)) daList = Assets.getText(path).trim().split('\n');
+
+		for (i in 0...daList.length)
+		{
+			daList[i] = daList[i].trim();
+		}
+
+		return daList;
+	}
+
 
 class ColorData {
       public var color:Int = 0xFFFFFFFF;
