e13fc51563fc3dd2bf62ddec89772f916ef9fa5f
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-03-03 01:25:30 +0100

    Update CharacterSelection.hx

--- source/CharacterSelection.hx
+++ source/CharacterSelection.hx
@@ -50,7 +50,7 @@ class CharacterSelection extends MusicBeatState
 
     override function create() 
     {
-        menuBG = new FlxSprite().loadGraphic(Paths.image('BG1'));
+        menuBG = new FlxSprite().loadGraphic('assets/images/BG1.png');
         menuBG.setGraphicSize(Std.int(menuBG.width * 1.1));
         menuBG.updateHitbox();
         menuBG.screenCenter();
@@ -113,7 +113,7 @@ class CharacterSelection extends MusicBeatState
         add(txtOptionTitle);
 
         if(curSelected >= menuItems.length) curSelected = 0;
-        menuBG.frames = Paths.getSparroAtlas('BG' + (curSelected + 1));
+        menuBG.loadGraphic('assets/images/BG' + (curSelected + 1) + '.png');
         changeSelection();
 
         cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
