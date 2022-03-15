ff75bb237daa8961dd2a5fd83208aeded7a88994
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-03-15 20:53:49 +0100

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -760,6 +760,12 @@ class PlayState extends MusicBeatState
 		                startCharacterPos(boyfriend);
 		                boyfriendGroup.add(boyfriend);
                 }
+
+                if(bfsel == 'bf') {
+                        boyfriend = new Boyfriend(0, 0, SONG.player1);
+		        startCharacterPos(boyfriend);
+		        boyfriendGroup.add(boyfriend);
+                }
 		
 		var camPos:FlxPoint = new FlxPoint(gf.getGraphicMidpoint().x, gf.getGraphicMidpoint().y);
 		camPos.x += gf.cameraPosition[0];
